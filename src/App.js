7e9ec5417a5776be6b7dcae039851685cd1d3dba
ricardo-applepie <ricardo@applepie.berlin>
2021-08-09 11:38:43 +0200

    created login and logout page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/login';
+import Logout from './components/logout';
 
+ 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload   .
         </p>
         <a
           className="App-link"
@@ -17,6 +20,9 @@ function App() {
         >
           Learn React
         </a>
+        <Login/>
+        <br/>
+        <Logout />
       </header>
     </div>
   );
